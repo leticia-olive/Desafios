ab5952cbcd95003e5f8921074b8ccd2b44a0a8c6
leticia-olive <leticiar@dbserver.com.br>
2024-04-29 13:46:59 -0300

    desafio soma valores

---
+++ src/SomaValores.java
@@ -0,0 +1,14 @@
+public class SomaValores {
+    /*Utilizando as estruturas de repetição , construa um algoritmo
+     que calcule a soma de valores inteiros existentes entre 1 e 100.
+     */
+    public static void main(String[] args) {
+        int s=0;
+        for (int cc = 1; cc <= 100; cc++)
+            s=s+cc;
+        System.out.println(s);
+
+
+
+    }
+}
