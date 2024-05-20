cc5078cfbeac92945f9d6d3fdd83415152a1c38a
leticia-olive <leticiar@dbserver.com.br>
2024-05-20 10:53:25 -0300

    ajuste desafio valores impares

--- src/ValoresImpares.java
+++ src/ValoresImpares.java
@@ -1,11 +1,12 @@
 public class ValoresImpares {
     public static void main(String[] args) {
-        int num[] = {1,2,3,4,5,6,7,8,9,10};
-        for (int valor: num){
-            if (valor%2!=0){
-                System.out.println(valor);
+        int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int quantidade = 0;
+        for (int valor : num) {
+            if (valor % 2 != 0) {
+                quantidade++;
             }
         }
-
+        System.out.println(quantidade);
     }
 }
