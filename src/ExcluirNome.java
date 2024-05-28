b2cce4cebb27d28b10a02132ac443180067131ff
leticia-olive <leticiar@dbserver.com.br>
2024-05-28 08:38:14 -0300

    sem conseguir ajustar

--- src/ExcluirNome.java
+++ src/ExcluirNome.java
@@ -1,23 +1,18 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ExcluirNome {
     public static void main(String[] args) {
-        ArrayList<String> nomes = new ArrayList<>();
-        int i;
+        ArrayList<String> nomes = new ArrayList<String>();
+        int i, j;
+        String igual;
 
-            nomes.add("pedro");
-            nomes.add ("joao");
-            nomes.add("pedro");
-            nomes.add("maria");
-            nomes.add("joaquim");
-            nomes.add("joao");
+        nomes.add("Joao");
+        nomes.add("Pedro");
+        nomes.add("Joao");
+        nomes.add("Pedro");
 
-        for (i=0;i<nomes.size();i++){
-            if (nomes.get(i).equals(nomes.get(i))){
-                nomes.remove(i);
-                break;
-            }
-        }
-            System.out.println(nomes);
+
     }}
